2c8e8294fdc6f736736b3c1c2a8b27776247a640
Nico <nimu80@gmail.com>
2020-04-03 18:12:38 +0200

    Added Square

--- src/main/java/ch/heigvd/gen/Square.java
+++ src/main/java/ch/heigvd/gen/Square.java
@@ -1,4 +1,28 @@
+/*
+ *  GEN - Laboratoire 03 - Bonzon Ludovic, Bourqui Denis, Müller Nicolas
+ *
+ *  Modified on 03.04.2020
+ */
+
 package ch.heigvd.gen;
 
 public class Square {
+
+    private String name;
+    private int positionInBoard;
+
+    public Square (String name, int positionInBoard) {
+
+        this.name = name;
+        this.positionInBoard = positionInBoard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPositionInBoard() {
+        return positionInBoard;
+    }
+
 }
--- src/test/java/ch/heigvd/gen/SquareTest.java
+++ src/test/java/ch/heigvd/gen/SquareTest.java
@@ -1,7 +1,29 @@
+/*
+ *  GEN - Laboratoire 03 - Bonzon Ludovic, Bourqui Denis, Müller Nicolas
+ *
+ *  Modified on 03.04.2020
+ */
+
 package ch.heigvd.gen;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest {
+
+    @Test
+    public void SquareNameShouldBeInitCorrectly() {
+
+        Square square = new Square("Test", 0);
+        assertEquals("Test", square.getName());
+    }
+
+    @Test
+    public void SquarePositionInBoardShouldInitCorrectly() {
+
+        Square square = new Square("Test", 0);
+        assertEquals(0, square.getPositionInBoard());
+    }
 
 }
