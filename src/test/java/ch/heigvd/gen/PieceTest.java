ba683bec157c5a11bec51229bc7fa067af5de827
Nico <nimu80@gmail.com>
2020-04-03 17:06:31 +0200

    Added piece

--- src/test/java/ch/heigvd/gen/PieceTest.java
+++ src/test/java/ch/heigvd/gen/PieceTest.java
@@ -2,6 +2,20 @@ package ch.heigvd.gen;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class PieceTest {
+
+    @Test
+    public void currentPositionShouldBeSetCorrectly() {
+        // TODO Can't test it without Square class and GO pos
+    }
+
+    @Test
+    public void nameShouldBeSetCorrectly() {
+
+        Piece piece = new Piece("this is great", new Square());
+        assertEquals("this is great", piece.getName());
+    }
 
 }
