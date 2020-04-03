85b23e163b2dddbcc9b51ba3fdc19d5cdca51216
Nico <nimu80@gmail.com>
2020-04-03 17:25:30 +0200

    Added Dice

--- src/test/java/ch/heigvd/gen/DiceTest.java
+++ src/test/java/ch/heigvd/gen/DiceTest.java
@@ -1,7 +1,24 @@
 package ch.heigvd.gen;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
+
+    @Test
+    public void numbersShouldBeBetweenOneAndSix() {
+
+        Dice dice = new Dice();
+
+        // 100000 should be enough to be sure
+        for (int i = 0; i < 100000; i++) {
+            dice.roll();
+            
+            if (dice.getFaceValue() > 6 || dice.getFaceValue() < 1) {
+                fail();
+            }
+        }
+    }
 
 }
